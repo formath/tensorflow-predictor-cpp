433d551a148d1e3cf19671d43171519be7ad0e1c
formath <jinpengliu@163.com>
2017-09-22 19:26:58 +0800

    bug

--- test/trans_data_to_tfrecord.sh
+++ test/trans_data_to_tfrecord.sh
@@ -4,4 +4,4 @@
 python2 ../dict.py '0' '9,6,116' '152,179' libfm.data dict.data
 
 # transform libfm data into tfrecord
-python2 ../dict.py dict.data '0' '9,6,116' '152,179' libfm.data libfm.tfrecord
+python2 ../data.py dict.data '0' '9,6,116' '152,179' libfm.data libfm.tfrecord
