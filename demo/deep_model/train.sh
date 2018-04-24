4ea7139c8f8f45b3a4a5b01f1c592802d5b53079
formath <jinpengliu@163.com>
2018-04-24 15:16:27 +0800

    fix

--- demo/deep_model/train.sh
+++ demo/deep_model/train.sh
@@ -13,6 +13,4 @@ python ../../python/train.py \
 # for feed Tensor when prediction
 python ../../python/predict_model.py \
     --dict "./data/dict.data" \
-    --sparse_fields "9,6,116,152,179" \
-    --train_file "./data/libfm.tfrecord" \
-    --valid_file "./data/libfm.tfrecord"
+    --sparse_fields "9,6,116,152,179"
